0b883940f0a6d79e2e343e616bdafbaa69e72788
Andre Wiggins <andrewiggins@live.com>
2019-07-02 20:38:42 -0700

    Add dev watch script

---
+++ scripts/dev.js
@@ -0,0 +1,88 @@
+const path = require("path");
+const { readdir } = require("fs").promises;
+const { spawn } = require("child_process");
+const { p } = require("./util");
+
+// https://github.com/mysticatea/npm-run-all/issues/105
+// https://git.io/fjKbw
+// Avoid MaxListenersExceededWarnings.
+process.stdout.setMaxListeners(0);
+process.stderr.setMaxListeners(0);
+
+/**
+ * @param {string} path
+ * @param {string[]} args
+ * @param {string} [cwd]
+ */
+function runNode(path, args, cwd = p()) {
+	args.unshift(path);
+	console.log("$", process.execPath, args.join(" "));
+	return spawn(process.execPath, args, {
+		stdio: "inherit",
+		cwd
+	});
+}
+
+function runYarn(scriptName, cwd = p()) {
+	// Largely inspired by npm-run-all source: https://git.io/fjKNS
+	const npmPath = process.env.npm_execpath;
+	const npmPathIsJs =
+		typeof npmPath === "string" && /\.m?js/.test(path.extname(npmPath));
+	const execPath = npmPathIsJs ? process.execPath : npmPath || "yarn";
+	const spawnArgs = ["run"];
+
+	if (npmPathIsJs) {
+		spawnArgs.unshift(npmPath);
+	}
+
+	spawnArgs.push(scriptName);
+
+	return spawn(execPath, spawnArgs, {
+		stdio: "inherit",
+		shell: !npmPathIsJs,
+		cwd
+	});
+}
+
+function watchScripts() {
+	runNode(p("node_modules/nodemon/bin/nodemon.js"), [
+		"--watch",
+		p("scripts"),
+		"--ignore",
+		p("scripts/components/index.js"),
+		p("scripts/build.js")
+	]);
+}
+
+function watchFramework(framework) {
+	runYarn("watch", p(`frameworks/${framework}`));
+}
+
+function startDevServer() {
+	runNode(p("node_modules/serve/bin/serve.js"), [p("dist")]);
+}
+
+async function main() {
+	const toWatch = process.argv.slice(2);
+	const availableFrameworks = await readdir(p("frameworks"));
+
+	const tasks = [];
+	if (toWatch.indexOf("scripts") !== -1) {
+		tasks.push(watchScripts);
+		toWatch.splice(toWatch.indexOf("scripts"), 1);
+	}
+
+	for (let framework of toWatch) {
+		if (availableFrameworks.indexOf(framework) !== -1) {
+			tasks.push(() => watchFramework(framework));
+		} else {
+			throw new Error(`Unknown framework given to watch: "${framework}"`);
+		}
+	}
+
+	tasks.push(startDevServer);
+
+	tasks.forEach(task => task());
+}
+
+main();
