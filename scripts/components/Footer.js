1e125d82117a445486f73ab5d6454aefa8d77df4
Andre Wiggins <andrewiggins@live.com>
2020-01-29 21:04:53 -0800

    Improve lighthouse score (a11y, best practices, SEO)

--- scripts/components/Footer.js
+++ scripts/components/Footer.js
@@ -4,11 +4,19 @@ export const Footer = () => (
 	<footer class="page-footer">
 		<p>
 			Built with <span class="text-error">♥</span> by{" "}
-			<a href="https://twitter.com/andre_wiggins" target="_blank">
+			<a
+				href="https://twitter.com/andre_wiggins"
+				target="_blank"
+				rel="noopener"
+			>
 				Andre Wiggins
 			</a>
 			. Styled using{" "}
-			<a href="https://picturepan2.github.io/spectre/" target="_blank">
+			<a
+				href="https://picturepan2.github.io/spectre/"
+				target="_blank"
+				rel="noopener"
+			>
 				Spectre.css
 			</a>
 		</p>
--- scripts/components/PageHeader.js
+++ scripts/components/PageHeader.js
@@ -5,7 +5,7 @@ import { h } from "preact";
  */
 export const PageHeader = ({ title, children }) => (
 	<div class="page-header">
-		<a class="off-canvas-toggle btn btn-primary btn-action" href="#sidebar">
+		<a class="off-canvas-toggle btn btn-primary btn-action" href="#sidebar" aria-label="Show navigation menu">
 			<i class="icon icon-menu" />
 		</a>
 		<h1 class="page-title">{title}</h1>
