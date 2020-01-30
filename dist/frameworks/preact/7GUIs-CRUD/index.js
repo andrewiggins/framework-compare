763ff16a4724c23b5797440952e2884f18022c5e
Andre Wiggins <andrewiggins@live.com>
2020-01-29 21:26:15 -0800

    Updates

---
+++ dist/frameworks/preact/7GUIs-CRUD/index.js
@@ -0,0 +1,366 @@
+(function () {
+  'use strict';
+
+  function _extends() {
+    _extends = Object.assign || function (target) {
+      for (var i = 1; i < arguments.length; i++) {
+        var source = arguments[i];
+
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+
+      return target;
+    };
+
+    return _extends.apply(this, arguments);
+  }
+
+  function _inheritsLoose(subClass, superClass) {
+    subClass.prototype = Object.create(superClass.prototype);
+    subClass.prototype.constructor = subClass;
+    subClass.__proto__ = superClass;
+  }
+
+  function _assertThisInitialized(self) {
+    if (self === void 0) {
+      throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
+
+    return self;
+  }
+
+  var n,u,t,i,r,o,f={},e=[],c=/acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|^--/i;function s(n,l){for(var u in l)n[u]=l[u];return n}function a(n){var l=n.parentNode;l&&l.removeChild(n);}function h(n,l,u){var t,i,r,o,f=arguments;if(l=s({},l),arguments.length>3)for(u=[u],t=3;t<arguments.length;t++)u.push(f[t]);if(null!=u&&(l.children=u),null!=n&&null!=n.defaultProps)for(i in n.defaultProps)void 0===l[i]&&(l[i]=n.defaultProps[i]);return o=l.key,null!=(r=l.ref)&&delete l.ref,null!=o&&delete l.key,v(n,l,o,r)}function v(l,u,t,i){var r={type:l,props:u,key:t,ref:i,__k:null,__p:null,__b:0,__e:null,l:null,__c:null,constructor:void 0};return n.vnode&&n.vnode(r),r}function d(n){return n.children}function y(n){if(null==n||"boolean"==typeof n)return null;if("string"==typeof n||"number"==typeof n)return v(null,n,null,null);if(null!=n.__e||null!=n.__c){var l=v(n.type,n.props,n.key,null);return l.__e=n.__e,l}return n}function m(n,l){this.props=n,this.context=l;}function w(n,l){if(null==l)return n.__p?w(n.__p,n.__p.__k.indexOf(n)+1):null;for(var u;l<n.__k.length;l++)if(null!=(u=n.__k[l])&&null!=u.__e)return u.__e;return "function"==typeof n.type?w(n):null}function g(n){var l,u;if(null!=(n=n.__p)&&null!=n.__c){for(n.__e=n.__c.base=null,l=0;l<n.__k.length;l++)if(null!=(u=n.__k[l])&&null!=u.__e){n.__e=n.__c.base=u.__e;break}return g(n)}}function k(l){(!l.__d&&(l.__d=!0)&&1===u.push(l)||i!==n.debounceRendering)&&(i=n.debounceRendering,(n.debounceRendering||t)(_));}function _(){var n,l,t,i,r,o,f,e;for(u.sort(function(n,l){return l.__v.__b-n.__v.__b});n=u.pop();)n.__d&&(t=void 0,i=void 0,o=(r=(l=n).__v).__e,f=l.__P,e=l.u,l.u=!1,f&&(t=[],i=$(f,r,s({},r),l.__n,void 0!==f.ownerSVGElement,null,t,e,null==o?w(r):o),j(t,r),i!=o&&g(r)));}function b(n,l,u,t,i,r,o,c,s){var h,v,p,d,y,m,g,k=u&&u.__k||e,_=k.length;if(c==f&&(c=null!=r?r[0]:_?w(u,0):null),h=0,l.__k=x(l.__k,function(u){if(null!=u){if(u.__p=l,u.__b=l.__b+1,null===(p=k[h])||p&&u.key==p.key&&u.type===p.type)k[h]=void 0;else for(v=0;v<_;v++){if((p=k[v])&&u.key==p.key&&u.type===p.type){k[v]=void 0;break}p=null;}if(d=$(n,u,p=p||f,t,i,r,o,null,c,s),(v=u.ref)&&p.ref!=v&&(g||(g=[])).push(v,u.__c||d,u),null!=d){if(null==m&&(m=d),null!=u.l)d=u.l,u.l=null;else if(r==p||d!=c||null==d.parentNode){n:if(null==c||c.parentNode!==n)n.appendChild(d);else{for(y=c,v=0;(y=y.nextSibling)&&v<_;v+=2)if(y==d)break n;n.insertBefore(d,c);}"option"==l.type&&(n.value="");}c=d.nextSibling,"function"==typeof l.type&&(l.l=d);}}return h++,u}),l.__e=m,null!=r&&"function"!=typeof l.type)for(h=r.length;h--;)null!=r[h]&&a(r[h]);for(h=_;h--;)null!=k[h]&&D(k[h],k[h]);if(g)for(h=0;h<g.length;h++)A(g[h],g[++h],g[++h]);}function x(n,l,u){if(null==u&&(u=[]),null==n||"boolean"==typeof n)l&&u.push(l(null));else if(Array.isArray(n))for(var t=0;t<n.length;t++)x(n[t],l,u);else u.push(l?l(y(n)):n);return u}function C(n,l,u,t,i){var r;for(r in u)r in l||N(n,r,null,u[r],t);for(r in l)i&&"function"!=typeof l[r]||"value"===r||"checked"===r||u[r]===l[r]||N(n,r,l[r],u[r],t);}function P(n,l,u){"-"===l[0]?n.setProperty(l,u):n[l]="number"==typeof u&&!1===c.test(l)?u+"px":null==u?"":u;}function N(n,l,u,t,i){var r,o,f,e,c;if("key"===(l=i?"className"===l?"class":l:"class"===l?"className":l)||"children"===l);else if("style"===l)if(r=n.style,"string"==typeof u)r.cssText=u;else{if("string"==typeof t&&(r.cssText="",t=null),t)for(o in t)u&&o in u||P(r,o,"");if(u)for(f in u)t&&u[f]===t[f]||P(r,f,u[f]);}else"o"===l[0]&&"n"===l[1]?(e=l!==(l=l.replace(/Capture$/,"")),c=l.toLowerCase(),l=(c in n?c:l).slice(2),u?(t||n.addEventListener(l,T,e),(n.t||(n.t={}))[l]=u):n.removeEventListener(l,T,e)):"list"!==l&&"tagName"!==l&&"form"!==l&&!i&&l in n?n[l]=null==u?"":u:"function"!=typeof u&&"dangerouslySetInnerHTML"!==l&&(l!==(l=l.replace(/^xlink:?/,""))?null==u||!1===u?n.removeAttributeNS("http://www.w3.org/1999/xlink",l.toLowerCase()):n.setAttributeNS("http://www.w3.org/1999/xlink",l.toLowerCase(),u):null==u||!1===u?n.removeAttribute(l):n.setAttribute(l,u));}function T(l){return this.t[l.type](n.event?n.event(l):l)}function $(l,u,t,i,r,o,f,e,c,a){var h,v,p,y,w,g,k,_,C,P,N=u.type;if(void 0!==u.constructor)return null;(h=n.__b)&&h(u);try{n:if("function"==typeof N){if(_=u.props,C=(h=N.contextType)&&i[h.__c],P=h?C?C.props.value:h.__p:i,t.__c?k=(v=u.__c=t.__c).__p=v.__E:("prototype"in N&&N.prototype.render?u.__c=v=new N(_,P):(u.__c=v=new m(_,P),v.constructor=N,v.render=H),C&&C.sub(v),v.props=_,v.state||(v.state={}),v.context=P,v.__n=i,p=v.__d=!0,v.__h=[]),null==v.__s&&(v.__s=v.state),null!=N.getDerivedStateFromProps&&s(v.__s==v.state?v.__s=s({},v.__s):v.__s,N.getDerivedStateFromProps(_,v.__s)),p)null==N.getDerivedStateFromProps&&null!=v.componentWillMount&&v.componentWillMount(),null!=v.componentDidMount&&f.push(v);else{if(null==N.getDerivedStateFromProps&&null==e&&null!=v.componentWillReceiveProps&&v.componentWillReceiveProps(_,P),!e&&null!=v.shouldComponentUpdate&&!1===v.shouldComponentUpdate(_,v.__s,P)){for(v.props=_,v.state=v.__s,v.__d=!1,v.__v=u,u.__e=null!=c?c!==t.__e?c:t.__e:null,u.__k=t.__k,h=0;h<u.__k.length;h++)u.__k[h]&&(u.__k[h].__p=u);break n}null!=v.componentWillUpdate&&v.componentWillUpdate(_,v.__s,P);}for(y=v.props,w=v.state,v.context=P,v.props=_,v.state=v.__s,(h=n.__r)&&h(u),v.__d=!1,v.__v=u,v.__P=l,h=v.render(v.props,v.state,v.context),u.__k=x(null!=h&&h.type==d&&null==h.key?h.props.children:h),null!=v.getChildContext&&(i=s(s({},i),v.getChildContext())),p||null==v.getSnapshotBeforeUpdate||(g=v.getSnapshotBeforeUpdate(y,w)),b(l,u,t,i,r,o,f,c,a),v.base=u.__e;h=v.__h.pop();)v.__s&&(v.state=v.__s),h.call(v);p||null==y||null==v.componentDidUpdate||v.componentDidUpdate(y,w,g),k&&(v.__E=v.__p=null);}else u.__e=z(t.__e,u,t,i,r,o,f,a);(h=n.diffed)&&h(u);}catch(l){n.__e(l,u,t);}return u.__e}function j(l,u){for(var t;t=l.pop();)try{t.componentDidMount();}catch(l){n.__e(l,t.__v);}n.__c&&n.__c(u);}function z(n,l,u,t,i,r,o,c){var s,a,h,v,p=u.props,d=l.props;if(i="svg"===l.type||i,null==n&&null!=r)for(s=0;s<r.length;s++)if(null!=(a=r[s])&&(null===l.type?3===a.nodeType:a.localName===l.type)){n=a,r[s]=null;break}if(null==n){if(null===l.type)return document.createTextNode(d);n=i?document.createElementNS("http://www.w3.org/2000/svg",l.type):document.createElement(l.type),r=null;}return null===l.type?p!==d&&(null!=r&&(r[r.indexOf(n)]=null),n.data=d):l!==u&&(null!=r&&(r=e.slice.call(n.childNodes)),h=(p=u.props||f).dangerouslySetInnerHTML,v=d.dangerouslySetInnerHTML,c||(v||h)&&(v&&h&&v.__html==h.__html||(n.innerHTML=v&&v.__html||"")),C(n,d,p,i,c),l.__k=l.props.children,v||b(n,l,u,t,"foreignObject"!==l.type&&i,r,o,f,c),c||("value"in d&&void 0!==d.value&&d.value!==n.value&&(n.value=null==d.value?"":d.value),"checked"in d&&void 0!==d.checked&&d.checked!==n.checked&&(n.checked=d.checked))),n}function A(l,u,t){try{"function"==typeof l?l(u):l.current=u;}catch(l){n.__e(l,t);}}function D(l,u,t){var i,r,o;if(n.unmount&&n.unmount(l),(i=l.ref)&&A(i,null,u),t||"function"==typeof l.type||(t=null!=(r=l.__e)),l.__e=l.l=null,null!=(i=l.__c)){if(i.componentWillUnmount)try{i.componentWillUnmount();}catch(l){n.__e(l,u);}i.base=i.__P=null;}if(i=l.__k)for(o=0;o<i.length;o++)i[o]&&D(i[o],u,t);null!=r&&a(r);}function H(n,l,u){return this.constructor(n,u)}function I(l,u,t){var i,o,c;n.__p&&n.__p(l,u),o=(i=t===r)?null:t&&t.__k||u.__k,l=h(d,null,[l]),c=[],$(u,i?u.__k=l:(t||u).__k=l,o||f,f,void 0!==u.ownerSVGElement,t&&!i?[t]:o?null:e.slice.call(u.childNodes),c,!1,t||f,i),j(c,l);}n={},m.prototype.setState=function(n,l){var u=this.__s!==this.state&&this.__s||(this.__s=s({},this.state));("function"!=typeof n||(n=n(u,this.props)))&&s(u,n),null!=n&&this.__v&&(this.u=!1,l&&this.__h.push(l),k(this));},m.prototype.forceUpdate=function(n){this.__v&&(n&&this.__h.push(n),this.u=!0,k(this));},m.prototype.render=d,u=[],t="function"==typeof Promise?Promise.prototype.then.bind(Promise.resolve()):setTimeout,i=n.debounceRendering,n.__e=function(n,l,u){for(var t;l=l.__p;)if((t=l.__c)&&!t.__p)try{if(t.constructor&&null!=t.constructor.getDerivedStateFromError)t.setState(t.constructor.getDerivedStateFromError(n));else{if(null==t.componentDidCatch)continue;t.componentDidCatch(n);}return k(t.__E=t)}catch(l){n=l;}throw n},r=f,o=0;
+
+  let id = 3;
+  /** @type {(url: string, options?: RequestInit) => Promise<any>} */
+
+  const mockFetch = window.mockFetch;
+  /**
+   * @template T
+   * @param {T} obj
+   * @returns {T}
+   */
+
+  function copy(obj) {
+    return JSON.parse(JSON.stringify(obj));
+  }
+
+  function getInitialState() {
+    return [{
+      id: 0,
+      name: "Hans",
+      surname: "Emil"
+    }, {
+      id: 1,
+      name: "Max",
+      surname: "Mustermann"
+    }, {
+      id: 2,
+      name: "Roman",
+      surname: "Tisch"
+    }];
+  }
+  function createApi() {
+    /**
+     * @typedef {{ id: number; name: string; surname: string; }} Person
+     * @type {Person[]}}
+     */
+    const store = getInitialState();
+
+    function getPersonById(id) {
+      for (let person of store) {
+        if (person.id == id) {
+          return person;
+        }
+      }
+
+      return null;
+    }
+
+    return {
+      /**
+       * @param {string} name
+       * @param {string} surname
+       * @returns {Promise<Person>}
+       */
+      create(name, surname) {
+        /** @type {Person} */
+        const person = {
+          id: id++,
+          name,
+          surname
+        };
+        return mockFetch("/create", {
+          method: "POST",
+          body: JSON.stringify(person)
+        }).then(() => copy(person));
+      },
+
+      /**
+       * @returns {Promise<Person[]>}
+       */
+      listAll() {
+        return mockFetch("/persons").then(() => copy(store));
+      },
+
+      /**
+       * @param {number} id
+       * @returns {Promise<Person>}
+       */
+      read(id) {
+        return mockFetch(`/persons/${id}`).then(() => copy(getPersonById(id)));
+      },
+
+      /**
+       * @param {number} id
+       * @param {string} name
+       * @param {string} surname
+       * @returns {Promise<Person>}
+       */
+      update(id, name, surname) {
+        const body = JSON.stringify({
+          name,
+          surname
+        });
+        return mockFetch(`/persons/${id}`, {
+          method: "POST",
+          body
+        }).then(() => {
+          const person = getPersonById(id);
+
+          if (person == null) {
+            throw new Error(`Could not find person with id "${id}".`);
+          }
+
+          person.name = name;
+          person.surname = surname;
+          return copy(person);
+        });
+      },
+
+      /**
+       * @param {number} id
+       * @returns {Promise<Person>}
+       */
+      delete(id) {
+        const person = getPersonById(id);
+
+        if (person == null) {
+          throw new Error(`Could not find person with id "${id}".`);
+        }
+
+        store.splice(store.indexOf(person), 1);
+        return mockFetch(`/persons/${id}`, {
+          method: "DELETE"
+        }).then(() => copy(person));
+      }
+
+    };
+  }
+
+  var api = createApi();
+
+  function Loading() {
+    return h("div", null, "Loading...");
+  }
+  /**
+   * @param {Person} person
+   */
+
+
+  function getDisplayName(person) {
+    return person.surname + ", " + person.name;
+  }
+
+  var EMPTY_PERSON = Object.freeze({
+    id: null,
+    name: "",
+    surname: ""
+  });
+  /**
+   * @typedef {{ id: number; name: string; surname: string; }} Person
+   * @typedef {{ initial: boolean; create: boolean; update: boolean; delete: boolean; }} LoadingState;
+   * @typedef {{ filter: string; name: string; surname: string; selectedPersonId: number; persons: Person[]; loading: LoadingState; }} State
+   * @returns {State}
+   */
+
+  function getInitialState$1() {
+    return {
+      filter: "",
+      name: "",
+      surname: "",
+      selectedPersonId: null,
+      persons: [],
+      loading: {
+        initial: true,
+        create: false,
+        update: false,
+        "delete": false
+      }
+    };
+  }
+  /**
+   * @param {{ type: string, payload: any; }} action
+   * @returns {(state: State) => State}
+   */
+
+
+  var reducer = function reducer(action) {
+    return function (state) {
+      // TODO: Consider if a reducer is really necessary here
+      switch (action.type) {
+        case "INITIAL_LOAD":
+          /** @type {Person} */
+          var person = action.payload.length ? action.payload[0] : EMPTY_PERSON;
+          return _extends({}, state, {
+            name: person.name,
+            surname: person.surname,
+            selectedPersonId: person.id,
+            persons: action.payload,
+            loading: _extends({}, state.loading, {
+              initial: false
+            })
+          });
+
+        case "UPDATE_FILTER":
+          return _extends({}, state, {
+            filter: action.payload
+          });
+
+        case "SELECT_PERSON":
+          var selectedPerson = state.persons.filter(function (person) {
+            return person.id == action.payload;
+          })[0] || EMPTY_PERSON;
+          return _extends({}, state, {
+            name: selectedPerson.name,
+            surname: selectedPerson.surname,
+            selectedPersonId: selectedPerson.id
+          });
+
+        default:
+          return state;
+      }
+    };
+  };
+
+  var App =
+  /*#__PURE__*/
+  function (_Component) {
+    _inheritsLoose(App, _Component);
+
+    function App(props, context) {
+      var _this;
+
+      _this = _Component.call(this, props, context) || this;
+      /** @type {State} */
+
+      _this.state = getInitialState$1();
+      _this.onFilterInput = _this.onFilterInput.bind(_assertThisInitialized(_this));
+      _this.onPersonSelect = _this.onPersonSelect.bind(_assertThisInitialized(_this));
+      return _this;
+    }
+
+    var _proto = App.prototype;
+
+    _proto.componentDidMount = function componentDidMount() {
+      var _this2 = this;
+
+      api.listAll().then(function (persons) {
+        _this2.setState(reducer({
+          type: "INITIAL_LOAD",
+          payload: persons
+        }));
+      });
+    };
+
+    _proto.onFilterInput = function onFilterInput(e) {
+      this.setState(reducer({
+        type: "UPDATE_FILTER",
+        payload: e.target.value
+      }));
+    };
+
+    _proto.onPersonSelect = function onPersonSelect(e) {
+      this.setState(reducer({
+        type: "SELECT_PERSON",
+        payload: e.target.value
+      }));
+    }
+    /**
+     * @param {{}} props
+     * @param {State} state
+     */
+    ;
+
+    _proto.render = function render(props, state) {
+      if (state.loading.initial) {
+        return h(Loading, null);
+      } // TODO: Consider if filteredPersons should be state so that the
+      // value of the options can be the index in the filteredPersons array
+      // and the selectedPerson is just the index in the filteredPersons array
+
+
+      var filteredPersons = state.persons;
+
+      if (state.filter) {
+        filteredPersons = state.persons.filter(function (person) {
+          return getDisplayName(person).toLowerCase().startsWith(state.filter.toLowerCase());
+        });
+      }
+
+      return h("div", {
+        "class": "crud-wrapper"
+      }, h("div", {
+        "class": "form-group"
+      }, h("label", null, "Filter prefix:", " ", h("input", {
+        type: "text",
+        value: state.filter,
+        onInput: this.onFilterInput
+      }))), h("div", {
+        "class": "form-group"
+      }, h("label", null, "Select a person to edit:"), h("select", {
+        size: 5,
+        onChange: this.onPersonSelect
+      }, filteredPersons.map(function (person) {
+        return h("option", {
+          key: person.id,
+          value: person.id,
+          selected: person.id == state.selectedPersonId
+        }, getDisplayName(person));
+      }))), h("div", null, h("div", {
+        "class": "form-group"
+      }, h("label", {
+        "for": "name"
+      }, "Name: "), h("input", {
+        id: "name",
+        type: "text",
+        value: state.name
+      })), h("div", {
+        "class": "form-group"
+      }, h("label", {
+        "for": "surname"
+      }, "Surname: "), h("input", {
+        id: "surname",
+        type: "text",
+        value: state.surname
+      }))), h("div", {
+        "class": "form-group"
+      }, h("button", {
+        type: "button"
+      }, "Create"), h("button", {
+        type: "button"
+      }, "Update"), h("button", {
+        type: "button"
+      }, "Delete")));
+    };
+
+    return App;
+  }(m);
+
+  I(h(App, null), document.getElementById("app"));
+
+}());
