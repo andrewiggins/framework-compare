a0a2a69c82da8a5035f0400f58f805dd61f3ca82
Andre Wiggins <andrewiggins@live.com>
2023-08-19 17:02:03 -0700

    Add keyboard shortcut to show hide fetch debugger

--- scripts/bundles/site.js
+++ scripts/bundles/site.js
@@ -7,24 +7,37 @@ import { installPolyfill } from "./controls/details-polyfill";
 import { createMockFetch, createMockFetchConfig } from "./controls/mockFetch";
 import { installFetchDebugger } from "./controls/mockFetchDebugger";
 
-window.createMockFetchConfig = createMockFetchConfig;
-window.createMockFetch = createMockFetch;
+function setupFetchDebugger() {
+	installFetchDebugger();
 
-window.mockFetchConfig = createMockFetchConfig();
-window.mockFetchConfig.log = (...msgs) => console.log(...msgs);
+	window.createMockFetchConfig = createMockFetchConfig;
+	window.createMockFetch = createMockFetch;
 
-window.mockFetch = createMockFetch(window.mockFetchConfig);
+	window.mockFetchConfig = createMockFetchConfig();
+	window.mockFetchConfig.log = (...msgs) => console.log(...msgs);
 
-installFetchDebugger();
+	window.mockFetch = createMockFetch(window.mockFetchConfig);
+
+	const fetchDebugger = document.createElement("mock-fetch-debugger");
+	fetchDebugger.config = window.mockFetchConfig;
+	window.fetchDebugger = fetchDebugger;
+
+	const draggableDialog = document.createElement("draggable-dialog");
+	draggableDialog.appendChild(fetchDebugger);
+	if (!location.href.toLowerCase().includes("7guis-crud")) {
+		draggableDialog.setAttribute("hidden", "");
+	}
+	document.body.appendChild(draggableDialog);
+
+	window.addEventListener("keypress", e => {
+		if (e.key === "d") {
+			draggableDialog.toggleAttribute("hidden");
+		}
+	});
+}
+
 installPolyfill();
 setupTabs();
 setupToggle();
 setupOffCanvas();
-
-const fetchDebugger = document.createElement("mock-fetch-debugger");
-fetchDebugger.config = window.mockFetchConfig;
-window.fetchDebugger = fetchDebugger;
-
-const draggableDialog = document.createElement("draggable-dialog");
-draggableDialog.appendChild(fetchDebugger);
-document.body.appendChild(draggableDialog);
+setupFetchDebugger();
